36f92b0cb081cc2a62c3dbddae0dc9fb2faa62e1
Lawrence R Kincheloe III <LokiMetaSmith@users.noreply.github.com>
2023-12-05 21:15:26 -0600

    Update script.sh
    updated node install script

--- script.sh
+++ script.sh
@@ -25,7 +25,13 @@ pip3 install ansible-core -y
 
 #installs for talk
 pip install piper-tts -y
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+#curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get update
 sudo apt-get install -y nodejs
 
 make CC=mpicc CXX=mpicxx LLAMA_MPI=1 -j
